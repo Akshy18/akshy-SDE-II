6336e0f142f744151ec8d547242702aeb0e7cca3
Akshy18 <jessiakshy36@gmail.com>
2025-05-01 20:09:09 +0530

    upadted cors

--- client/src/components/Api.jsx
+++ client/src/components/Api.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 // Create base axios instance with increased timeout
 const api = axios.create({
-  baseURL: "http://localhost:5000/api" ,
+  baseURL: import.meta.env.VITE_API_BASE_URL ,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
