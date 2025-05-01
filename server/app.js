5f9ab37e48bfa4575fedfbac8a433374f457306b
Akshy18 <jessiakshy36@gmail.com>
2025-05-01 18:47:38 +0530

    upadted cors

--- server/app.js
+++ server/app.js
@@ -17,8 +17,11 @@ app.use(helmet({
 }));
 
 // CORS configuration for frontend access
+const FRONTEND_URL = process.env.NODE_ENV === 'production'
+  ? 'https://fullstack-task-akshy-1.onrender.com/'
+  : 'http://localhost:5173';
 app.use(cors({
-  origin: 'http://localhost:5173', // Frontend origin
+  origin: FRONTEND_URL, // Frontend origin
   credentials: true, // Allow credentials/cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
